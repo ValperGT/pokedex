6e1b2aa2f55ae379b3bab15e23e56b4bbd0e0f4c
Juan Pablo Dulce <86208054+ValperGT@users.noreply.github.com>
2021-09-11 16:36:06 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/";
 const $pokemonContainer = document.querySelector(".pokemon-container");
+const $pokemonContainer2 = document.querySelector(".pokemon-container2");
 const $pokemonInput = document.querySelector(".pokemon-input");
 const $pokemonButton = document.querySelector(".pokemon-button");
 
 $pokemonButton.addEventListener("click", () => {
+    $pokemonContainer.classList.add("show");
+    $pokemonContainer2.classList.add("show");
     fetch(`${POKEMON_URL}${$pokemonInput.value}`)
         .then(response => {
             if(response.ok){
@@ -19,9 +22,9 @@ $pokemonButton.addEventListener("click", () => {
             })
             $pokemonContainer.innerHTML = `
             <p class="pokemon-name">${pokemon.name}</p>
+            <img width="200px" height="200px" src="${pokemon.sprites.front_default}" class="pokemon-image">
             <p>Hp: ${pokemon.stats[0].base_stat}</p>
             <p>Type: ${pokemonTypes}</p>
-            <img src="${pokemon.sprites.other.dream_world.front_default}" class="pokemon-image">
             `
         })
         .catch(error => console.log(error))
